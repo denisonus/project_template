3cfe13a7fec9752987814ece162d02cd2fb1b14c
Denys Solomennyi <denisonovich@gmail.com>
2024-03-11 16:51:59 +0200

    created empty functions

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+def input_from_console():
+    pass
+
+
+def read_file_python():
+    pass
+
+
+def read_file_pandas():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,6 @@
+def output_to_console():
+    pass
+
+
+def write_file_python():
+    pass
